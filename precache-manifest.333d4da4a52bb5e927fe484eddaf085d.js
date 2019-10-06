dae71dd57e154d7592c5a87b77af53736589969b
catai <catai@edu.uwaterloo.ca>
2019-10-06 15:29:55 -0400

    Updates

---
+++ precache-manifest.333d4da4a52bb5e927fe484eddaf085d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3ea0f54f119014a55e6451ca14f52b9",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "2f57084ef18df4d86cdb",
+    "url": "/meme-generator/static/css/main.7b321823.chunk.css"
+  },
+  {
+    "revision": "648279e622d74641bb11",
+    "url": "/meme-generator/static/js/2.f55b3340.chunk.js"
+  },
+  {
+    "revision": "2f57084ef18df4d86cdb",
+    "url": "/meme-generator/static/js/main.0a1361d2.chunk.js"
+  },
+  {
+    "revision": "46cc4588d4e0c1683030",
+    "url": "/meme-generator/static/js/runtime-main.7ad0fd18.js"
+  }
+]);
