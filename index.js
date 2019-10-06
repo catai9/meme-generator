edd83878610ecb0515b84520dd6fd71701178462
catai <catai@edu.uwaterloo.ca>
2019-10-06 15:25:08 -0400

    move

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './style.scss';
-import App from './src/App';
-
-ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, document.getElementById("root"));
